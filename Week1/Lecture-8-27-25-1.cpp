96c127cdaa86189f9de445414cb591c3a03c4391
JulianMousterou <julian.mousterou@gmail.com>
2025-08-27 20:44:50 -0400

    Create Lecture-8-27-25-1.cpp

---
+++ Week1/Lecture-8-27-25-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+double sum(double a, double b){
+        double sum1 = a + b; 
+        return sum1; 
+}
+
+int main(){
+        double number = sum(1,2); 
+        std::cout<<number;
+
+
+} 
